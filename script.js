10c39ff2e5cb6cc73de8b75570501d3869db11c4
frantic-dev <107622593+frantic-dev@users.noreply.github.com>
2022-08-07 13:22:05 +0100

    get value of each input

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 let myLibrary = [];
 
+const title = document.querySelector('#title');
+let titleInput = "";
+
+title.addEventListener('keydown', (e) => {
+    return titleInput += e.key
+});
+
+const author = document.querySelector('#author');
+let authorInput = '';
+
+author.addEventListener('keydown', (e) => {
+    return authorInput += e.key
+});
+const genre = document.querySelector('#genre');
+let genreInput = '';
+
+genre.addEventListener('keydown', (e) => {
+    return genreInput += e.key
+});
+const pages = document.querySelector('#pages');
+let pagesInput = '';
+
+pages.addEventListener('keydown', (e) => {
+    return pagesInput += e.key
+});
